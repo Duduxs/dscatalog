31fd572253e219f7b846af8dc927c1c424ac5135
Duduxs <duduxss3@gmail.com>
2020-09-30 20:23:19 -0300

    Database seeding

--- backend/src/main/java/com/edudev/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/edudev/dscatalog/services/CategoryService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.edudev.dscatalog.entities.Category;
 import com.edudev.dscatalog.exceptions.idNotFoundException;
@@ -16,12 +17,13 @@ public class CategoryService {
 	@Autowired
 	CategoryRepository categoryRepository;
 	
-
+	@Transactional(readOnly = true)
 	public Category findByid(Long id) {
 		Optional<Category> category = categoryRepository.findById(id);
 		return category.orElseThrow(() -> new idNotFoundException("Id not found!"));
 	}
 	
+	@Transactional(readOnly = true)
 	public List<Category> findAll(){
 		return categoryRepository.findAll();
 	}
