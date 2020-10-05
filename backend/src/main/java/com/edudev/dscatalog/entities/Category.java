2b7e3c9b123be549f82638204f54265e2a793b27
Duduxs <duduxss3@gmail.com>
2020-10-05 13:28:27 -0300

    Product Entity, SQL import

--- backend/src/main/java/com/edudev/dscatalog/entities/Category.java
+++ backend/src/main/java/com/edudev/dscatalog/entities/Category.java
@@ -62,7 +62,7 @@ public class Category implements Serializable {
 	public Instant getUpdatedAt() {
 		return updatedAt;
 	}
-	
+
 	@PrePersist
 	public void prePersist() {
 		createdAt = Instant.now();
@@ -72,6 +72,7 @@ public class Category implements Serializable {
 	public void preUpdate() {
 		updatedAt = Instant.now();
 	}
+	
 
 	@Override
 	public int hashCode() {
